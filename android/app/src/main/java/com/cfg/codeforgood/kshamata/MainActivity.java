0735fdfb3b570cb92f3b1fe009ce8c12faee8b66
DCODIE <djdimplejd@gmail.com>
2017-07-08 15:09:49 +0530

    Login page activity added

--- android/app/src/main/java/com/cfg/codeforgood/kshamata/MainActivity.java
+++ android/app/src/main/java/com/cfg/codeforgood/kshamata/MainActivity.java
@@ -1,7 +1,7 @@
 package com.cfg.codeforgood.kshamata;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
