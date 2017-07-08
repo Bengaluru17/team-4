1add2b411bcec2ad3366665aa55b43f9fb169c97
DCODIE <djdimplejd@gmail.com>
2017-07-09 03:20:25 +0530

    kshamata app-with map at specific lat n long

--- kshamata/app/src/main/java/com/example/dimpledoshi/kshamata/MapsActivity.java
+++ kshamata/app/src/main/java/com/example/dimpledoshi/kshamata/MapsActivity.java
@@ -15,7 +15,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     private GoogleMap mMap;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps);
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
