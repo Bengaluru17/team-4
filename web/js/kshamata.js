fcd4fb758faaf9763517fe5fbbc5bfb858112116
Amruth Pillai <im.amruth@gmail.com>
2017-07-08 14:30:17 +0530

    added angularjs

---
+++ web/js/kshamata.js
@@ -0,0 +1,10 @@
+// Initialize Firebase
+var config = {
+  apiKey: "AIzaSyAeO9c2_BnWJoJmckhDu4Ra3BYSzfLhdNQ",
+  authDomain: "kshamata-68f54.firebaseapp.com",
+  databaseURL: "https://kshamata-68f54.firebaseio.com",
+  projectId: "kshamata-68f54",
+  storageBucket: "kshamata-68f54.appspot.com",
+  messagingSenderId: "813353117552"
+};
+firebase.initializeApp(config);
