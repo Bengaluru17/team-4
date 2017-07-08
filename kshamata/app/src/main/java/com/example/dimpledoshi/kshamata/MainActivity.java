b5d493578be2cf1165569a9ede8c37e4a538a00a
DCODIE <djdimplejd@gmail.com>
2017-07-08 15:16:25 +0530

    Login page activity added

---
+++ kshamata/app/src/main/java/com/example/dimpledoshi/kshamata/MainActivity.java
@@ -0,0 +1,30 @@
+package com.example.dimpledoshi.kshamata;
+
+import android.net.Uri;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.google.android.gms.appindexing.Action;
+import com.google.android.gms.appindexing.AppIndex;
+import com.google.android.gms.appindexing.Thing;
+import com.google.android.gms.common.api.GoogleApiClient;
+
+public class MainActivity extends AppCompatActivity {
+
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+
+    }
+
+
+    }
+}
